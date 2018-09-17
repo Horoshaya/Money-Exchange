9ac8f531a548e3e755d63d7cd63128a37f566b3e
Дарья <daria@MBP-Dara.lan>
2018-09-18 01:00:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+			var exchange = {};
+
+			if (currency >= 10000) {
+				exchange.error = "You are rich, my friend! We don't have so much coins for exchange";
+				return exchange;
+			} else if (currency <= 0) {
+				return exchange;
+			}
+
+			if (Math.fround(currency / 50) >= 1) {
+				if (Math.fround(currency / 50) == Math.floor(currency / 50)) {
+					exchange.H = Math.floor(currency / 50);	
+					return exchange;		
+				} else {
+					exchange.H = Math.floor(currency / 50);
+					currency = currency - 50 * exchange.H;
+				}
+			} if (Math.fround(currency / 25) >= 1) {
+				if (Math.fround(currency / 25) == Math.floor(currency / 25)) {
+					exchange.Q = Math.floor(currency / 25);
+					return exchange;		
+				} else {
+					exchange.Q = Math.floor(currency / 25);
+					currency = currency - 25 * exchange.Q;
+				}
+			
+			} if (Math.fround(currency / 10) >= 1) {
+				if (Math.fround(currency / 10) == Math.floor(currency / 10)) {
+					exchange.D = Math.floor(currency / 10);
+					return exchange;			
+				} else {
+					exchange.D = Math.floor(currency / 10);
+					currency = currency - 10 * exchange.D;
+				}
+			} if (Math.fround(currency / 5) >= 1) {
+				if (Math.fround(currency / 5) == Math.floor(currency / 5)) {
+					exchange.N = Math.floor(currency / 5);
+					return exchange;			
+				} else {
+					exchange.N = Math.floor(currency / 5);
+					currency = currency - 5 * exchange.N;
+				}
+			} if (Math.fround(currency / 1) >= 1) {
+				if (Math.fround(currency / 1) == Math.floor(currency / 1)) {
+					exchange.P = Math.floor(currency / 1);
+					return exchange;			
+				} else {
+					exchange.P = Math.floor(currency / 1);
+					currency = currency - 1 * exchange.P;
+				}
+			}
+			return exchange;	
 }
